ecdbc5b0797f8d0567079684c79a0601dd368699
unknown <tjdgus9684@pearlabyss.com>
2025-04-16 18:56:58 +0900

    add debuging for updateAngles

--- Src/IKManager.cpp
+++ Src/IKManager.cpp
@@ -414,6 +414,7 @@ void IKManager::updateAngle()
 			XMVECTOR nowQuat = XMLoadFloat4(&m_nowRotation[bone.idx]);
 			XMVECTOR newQuat = XMQuaternionMultiply(quat, nowQuat);
 			XMStoreFloat4(&m_nowRotation[bone.idx], newQuat);
+			p("\n");
 		}
 	}
 }
